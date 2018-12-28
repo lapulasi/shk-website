5e2fe1f22c7bc76a6e41a7799a76270c21e44e06
听夏 <laplace@tingxiadeMacBook-Air.local>
2018-12-28 16:30:49 +0800

    change vue-swiper bug

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -368,12 +368,10 @@
           slidesPerView: 4,
           spaceBetween: 54,
           slidesPerGroup: 4,
-          loop: true,
+          loop: false,
           loopFillGroupWithBlank: true,
-          pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-          }
+          pagination: '.swiper-pagination',
+          paginationClickable :true
         },
         hederShow: false,
         imgSrc: logoUrlG
